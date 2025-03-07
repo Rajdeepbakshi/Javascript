955547ddc6b90284ddc03b4f36b0cd01b32d4ce6
Rajdeep Bakshi <rajdeepbakshi565@gmail.com>
2025-03-07 18:35:55 +0530

    changed the code

--- Scope/scopedem1.js
+++ Scope/scopedem1.js
@@ -3,9 +3,13 @@ var teacher = "Rajdeep Bakshi"; // -------> Global Scope
 function fun() { //---------> Global Scope as it is not in any other function
     var teacher = "Raju";
     console.log("hello", teacher);
+}
 
     function gun() {
         var student ="Rahul";
         console.log("welcome to the class", student);
-    }
+    
 }
+
+fun();
+gun();
