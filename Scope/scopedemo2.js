9d8b304739953392b6ab5c4b987ce456851fdca5
Rajdeep Bakshi <rajdeepbakshi565@gmail.com>
2025-03-07 18:38:21 +0530

    new file:   Scope/scopedemo2.js

---
+++ Scope/scopedemo2.js
@@ -0,0 +1,15 @@
+var teacher = "Rajdeep Bakshi"; // -------> Global Scope
+
+function fun() { //---------> Global Scope as it is not in any other function
+    var teacher = "Raju";
+    console.log("hello", teacher);
+}
+
+    function gun() {
+        var student ="Rahul";
+        console.log("welcome to the class", student, "Teacher for your class is", teacher);
+    
+}
+
+fun();
+gun();
