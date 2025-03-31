5cee6cf41b6ac71ca5f930b25f08ce9f3a832153
Rajdeep Bakshi <rajdeepbakshi565@gmail.com>
2025-04-01 01:43:21 +0530

    javascript

--- Advanced Js Functions/function.js
+++ Advanced Js Functions/function.js
@@ -29,11 +29,30 @@ const result = addTwoNumbers(2, 3); //3,4 are arguments
 
 function loginUserMessage(username) {
     if(username === undefined) {
-        console.log("Please enter your username");
+  //      console.log("Please enter your username");
         return
     }
     return `${username}, you are logged in`
 }
 
-console.log(loginUserMessage("Rajdeep")); // Rajdeep, you are logged in
-console.log(loginUserMessage()); // Please enter your username
+//console.log(loginUserMessage("Rajdeep")); // Rajdeep, you are logged in
+//console.log(loginUserMessage()); // Please enter your username
+
+function calculateCartPrice(...num1) { //if we add val1, val2 so the output will be 500 as 200 goes for val1 and 400 goes for val2
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 500))
+
+
+const user = {
+    username: "Rajdeep",
+    price: 200
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+handleObject(user)
