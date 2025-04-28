6710ed284390dca20231efe423edf1c55475b2dd
Rajdeep Bakshi <rajdeepbakshi565@gmail.com>
2025-04-29 02:43:09 +0530

    javascript

--- control_flow/if.js
+++ control_flow/if.js
@@ -85,10 +85,50 @@
 // }
 
 
-let age = 19
+// let age = 19
 
-if(age>=18){
-    console.log("You will get the driving license");
-} else {
-    console.log("You will not get the driving license");
+// if(age>=18){
+//     console.log("You will get the driving license");
+// } else {
+//     console.log("You will not get the driving license");
+// }
+
+//if
+
+// const temperature = 41
+
+// if(temperature < 50) {
+//     console.log("less than 50");
+// }
+// else {
+//     console.log("temperature is greater than 50");
+// }
+
+// const score = 200
+
+// if(score > 100) {
+//     const power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+// console.log(`User Power: ${power}`);
+
+// const balance = 1000
+// // if(balance > 500) console.log("test");
+// if(balance < 500) {
+//     console.log("less than 500");
+// } else if(balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard) {
+    console.log("You can buy the product");
 }
+
+
