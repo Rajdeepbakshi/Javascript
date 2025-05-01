85e566a16e7c8c876237804009bfb0d4b59d312f
Rajdeep Bakshi <rajdeepbakshi565@gmail.com>
2025-05-02 01:58:36 +0530

    javascript

--- control_flow/for.js
+++ control_flow/for.js
@@ -6,3 +6,13 @@ for(userScore; userScore>=minValue; userScore = userScore-1){
     }
     console.log(`UserScore is ${userScore}`)
 }
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`Outer loop value : ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(i+ " * " + j + " = " + i*j);
+    }
+}
+
+
