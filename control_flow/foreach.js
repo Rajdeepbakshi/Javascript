98c043ea64775b1530cb264922bf42e0afd90691
Rajdeep Bakshi <rajdeepbakshi565@gmail.com>
2025-05-04 02:35:49 +0530

    javascript

---
+++ control_flow/foreach.js
@@ -0,0 +1,22 @@
+ const coding = ["JavaScript", "Python", "Java", "C++", "Ruby"];
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// coding.forEach( (item)=> {
+//     console.log(item);
+// } )
+
+// function printMe(item) {
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr);
+    
+} )
+
--- control_flow/forof.js
+++ control_flow/forof.js
@@ -1,1 +1,34 @@
 //for of
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) {
+    //console.log(num);
+    
+}
+
+const greetings = "Hello World";
+
+for (const greet of greetings) {
+    if (greet === " ") {
+        continue;
+    }
+    //console.log(`Each char is ${greet}`);
+    
+}
+
+
+
+// Maps
+
+const map = new Map() // maps are known for unique values; duplicate values are not allowed
+map.set("IN", "India")
+map.set("US", "United States")
+map.set("UK", "United Kingdom")
+
+//console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+    
