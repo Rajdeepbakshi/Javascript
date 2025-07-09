9ec4fcb1740310f7cfa667ec4ad448af29434c13
Rajdeep Bakshi <rajdeepbakshi565@gmail.com>
2025-07-09 17:46:55 +0530

    javascript

---
+++ events/one.js
@@ -0,0 +1,16 @@
+// document.getElementById('owl').onclick = function() {
+//     alert('You clicked the owl!');
+// }
+
+// document.getElementById('owl').addEventListener('click', function() {
+//     alert('You clicked the owl!');
+// });   ----------------------------------------------------> Correct one 
+
+
+
+// type, timestamp, defaultPrevented
+// target, toElement, srcElement, currentTarget
+// clientX, clientY, screenX, screenY
+// altkey, ctrlkey, shiftkey, keyCode
+
+// for false case in the event listener, it is called event bubbling
